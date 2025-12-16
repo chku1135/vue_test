06f03e1c2f5f79dfaf47420d511437c36c8fc124
chku1135 <109785527+chku1135@users.noreply.github.com>
2025-12-16 16:35:39 +0900

    Add files via upload

--- src/service/LoginAPI.js
+++ src/service/LoginAPI.js
@@ -1,23 +1,33 @@
+import axios from "axios"
+
 const getUserInfo = (userId, userPw) => {
     const reqData = {
         'user_id': userId,
         'user_pw': userPw
     }
 
-    return {
-        'data': {
-            'user_id': reqData.user_id,
-            'user_token': 'user_test_token',
-            'user_role': 'ADM'
+    let serverUrl = '//localhost:8081'
+
+    return axios.post(serverUrl + '/user/login', reqData, {
+        headers: {
+            'Content-type': 'application/json'
         }
-    }
+    })
+
+    // return {
+    //     'data': {
+    //         'user_id': reqData.user_id,
+    //         'user_token': 'user_test_token',
+    //         'user_role': 'ADM'
+    //     }
+    // }
 }
 
 export default {
     async doLogin(user_id, userPw) {
         try{
             const getUserInfoPromise = getUserInfo(userId, userPw)
-            const [userInfoResponse] = await promise.all([getUserInfoPromise])
+            const [userInfoResponse] = await Promise.all([getUserInfoPromise])
             if (userInfoResponse.data.length === 0 ){
                 return 'notFound'
             } else {
--- src/views/common/Login.vue
+++ src/views/common/Login.vue
@@ -44,21 +44,36 @@ export default {
       }
 
       //로그인 API 호출
+      // try {
+      //   let loginResult = await this.login({user_id: this.user_id_id, user_pw: this.user_pw})
+      //   if (loginResult) alert('로그인 결과 : ' + loginResult)
+      // } catch (err) {
+      //   if (err.message.indexOf('Network Error') > -1) {
+      //     alert('서버에 접속할 수 없습니다. 상태를 확인해 주세요.')
+      //   } else {
+      //     alert('로그인 정보를 확인할 수 없습니다.')
+      //   }
+      // }
+      // alert('로그인 되었습니다.')
       try {
-        let loginResult = await this.login({user_id: this.user_id_id, user_pw: this.user_pw})
-        if (loginResult) alert('로그인 결과 : ' + loginResult)
+        let loginResult = await this.login({user_id: this.user_id, user_pw: this.user_pw})
+        if (loginResult) this.goToPages()
       } catch (err) {
         if (err.message.indexOf('Network Error') > -1) {
-          alert('서버에 접속할 수 없습니다. 상태를 확인해 주세요.')
+          alert('서버에 접속할 수 없습니다. 상태를 확인해주세요.')
         } else {
           alert('로그인 정보를 확인할 수 없습니다.')
         }
       }
-      // alert('로그인 되었습니다.')
+    },
+    goToPages(){
+      this.$router.push({
+        name: 'List'
+      })
     }
   },
   computed: {
-    ...getters({
+    ...mapGetters({
       errorState: 'getErrorState'
     })
   }
